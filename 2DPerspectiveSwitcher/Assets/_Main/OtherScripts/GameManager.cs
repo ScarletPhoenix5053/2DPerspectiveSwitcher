72c557a65d74f1d1b9bd8ff5a37e0012eafbb0a6
ScarletPhoenix5053 <45509046+ScarletPhoenix5053@users.noreply.github.com>
2018-12-02 13:57:49 +1300

    Initial commit

---
+++ 2DPerspectiveSwitcher/Assets/_Main/OtherScripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System;
+using Sierra.PerspectiveSwitcher2D;
+
+public class GameManager : MonoBehaviour
+{
+    public PerspectiveSwitcher PerspectiveSwitcher;
+
+    private bool CamPerspAlternate = true;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            SetAlternatingCamPersp(PerspectiveSwitcher.CubePerspective.front, PerspectiveSwitcher.CubePerspective.back);
+        }
+    }
+
+    private void SetRandomCamPersp()
+    {
+        int newPersp = UnityEngine.Random.Range(1, 7);
+        switch (newPersp)
+        {
+            case 1:
+                PerspectiveSwitcher.SetPerspective(PerspectiveSwitcher.CubePerspective.top);
+                break;
+            case 2:
+                PerspectiveSwitcher.SetPerspective(PerspectiveSwitcher.CubePerspective.bottom);
+                break;
+            case 3:
+                PerspectiveSwitcher.SetPerspective(PerspectiveSwitcher.CubePerspective.left);
+                break;
+            case 4:
+                PerspectiveSwitcher.SetPerspective(PerspectiveSwitcher.CubePerspective.right);
+                break;
+            case 5:
+                PerspectiveSwitcher.SetPerspective(PerspectiveSwitcher.CubePerspective.front);
+                break;
+            case 6:
+                PerspectiveSwitcher.SetPerspective(PerspectiveSwitcher.CubePerspective.back);
+                break;
+            default:
+                break;
+        }
+    }
+    private void SetAlternatingCamPersp(PerspectiveSwitcher.CubePerspective persp1, PerspectiveSwitcher.CubePerspective persp2)
+    {        
+        if (CamPerspAlternate)
+        {
+            PerspectiveSwitcher.SetPerspective(persp1);
+
+            CamPerspAlternate = false;
+        }
+        else
+        {
+            PerspectiveSwitcher.SetPerspective(persp2);
+
+            CamPerspAlternate = true;
+        }
+    }
+}
